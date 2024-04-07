9ba2d765e086ef9c262c53d60c5457f8bca72f32
Muhammad Yunus <muhammadyunus1072@gmail.com>
2024-04-07 11:40:29 +0700

    generate

--- resources/views/livewire/page/scanner.blade.php
+++ resources/views/livewire/page/scanner.blade.php
@@ -8,23 +8,28 @@
 
 @push('js')
     <script type="text/javascript">
-    let scanner = new Instascan.Scanner({ 
-        continuous: true,
-        video: document.getElementById('preview') ,
-        refractoryPeriod: 30000,
-    });
-    scanner.addListener('scan', function (content) {
-        Livewire.dispatch('on-scanned', { data: content })
-    });
-    Instascan.Camera.getCameras().then(function (cameras) {
-        if (cameras.length > 0) {
-        scanner.start(cameras[0]);
-        } else {
-        console.error('No cameras found.');
-        }
-    }).catch(function (e) {
-        console.error(e);
-    });
-    // scanner.stop();
+        Instascan.Camera.getCameras().then(function (cameras) {
+            // Find the back camera
+            let backCamera = cameras.find(camera => camera.name.toLowerCase().includes('back'));
+
+            // If back camera found, start scanning with it
+            if (backCamera) {
+                let scanner = new Instascan.Scanner({ 
+                    continuous: true,
+                    video: document.getElementById('preview'),
+                    refractoryPeriod: 30000,
+                });
+
+                scanner.addListener('scan', function (content) {
+                    Livewire.dispatch('on-scanned', { data: content });
+                });
+
+                scanner.start(backCamera);
+            } else {
+                console.error('No back camera found.');
+            }
+        }).catch(function (e) {
+            console.error(e);
+        });
     </script>
 @endpush
