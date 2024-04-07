6bf13812ed7b5c4e9ecaab8ab163d3fa8cc5cb59
Muhammad Yunus <muhammadyunus1072@gmail.com>
2024-04-07 23:54:27 +0700

    fix name n logo

--- resources/views/app/page/qrcode.blade.php
+++ resources/views/app/page/qrcode.blade.php
@@ -6,7 +6,7 @@
 
 <div class="w-100" style="position: relative;">
     <img class="theme-light-show" style="width: 100vw; position: absolute; left: 0; top: 0;"
-    src="{{ asset('files/images/barcode.png') }}" alt="" />
+    src="{{ asset('files/images/barcode_new.png') }}" alt="" />
     <div class="row d-flex justify-content-center mt-5">
         <!--begin::Aside-->
         <div class="col-10 col-md-4 row d-flex justify-content-center" style="margin-top: 10%;">
