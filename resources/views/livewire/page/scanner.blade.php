62ce545f74a6d4f826a3668eb8ab43c788ab8c02
Muhammad Yunus <muhammadyunus1072@gmail.com>
2024-04-07 10:59:02 +0700

    scanner ON

--- resources/views/livewire/page/scanner.blade.php
+++ resources/views/livewire/page/scanner.blade.php
@@ -18,13 +18,13 @@
     });
     Instascan.Camera.getCameras().then(function (cameras) {
         if (cameras.length > 0) {
-        // scanner.start(cameras[0]);
+        scanner.start(cameras[0]);
         } else {
         console.error('No cameras found.');
         }
     }).catch(function (e) {
         console.error(e);
     });
-    scanner.stop();
+    // scanner.stop();
     </script>
 @endpush
