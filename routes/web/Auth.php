835a4cfc88348a2f0ac2ddd54e69e21981b1b602
Muhammad Yunus <muhammadyunus1072@gmail.com>
2024-04-08 20:51:34 +0700

    fix

--- routes/web/Auth.php
+++ routes/web/Auth.php
@@ -6,10 +6,6 @@ use App\Http\Controllers\Auth\AuthController;
 Route::middleware('guest')->group(function () {
     Route::get("/admin/login", [AuthController::class, "login"])->name("login");
     Route::get("/logout", [AuthController::class, "logout"])->name('logout');
-    Route::get('/forgot-password', [AuthController::class, 'forgotPassword'])->name('password.request');
-    Route::get('/reset-password/{token}',  [AuthController::class, 'resetPassword'])->name('password.reset');
-    Route::get("/email_verification", [AuthController::class, "emailVerification"])->name("verification.index");
-    Route::get('/email/verify/{id}/{hash}', [AuthController::class, "emailVerificationVerify"])->middleware('signed')->name('verification.verify');
 });
 
 Route::middleware('auth')->group(function () {
