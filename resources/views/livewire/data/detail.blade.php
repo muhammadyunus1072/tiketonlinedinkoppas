07f0016aaae0d97163e687bdaca6db96d215a770
Muhammad Yunus <muhammadyunus1072@gmail.com>
2024-09-28 20:00:21 +0700

    data-api

--- resources/views/livewire/data/detail.blade.php
+++ resources/views/livewire/data/detail.blade.php
@@ -1,18 +1,32 @@
-<form wire:submit="store">
-    <div class='row'>
-        <div class="col-md-6 mb-4">
-            <label>Data</label>
-            <input type="text" class="form-control @error('data') is-invalid @enderror" wire:model.blur="data" />
-
-            @error('data')
-                <div class="invalid-feedback">
-                    {{ $message }}
-                </div>
-            @enderror
-        </div>
+<form wire:submit="store" class="form w-100" novalidate="novalidate">
+    @csrf
+   
+    <!--end::Separator-->
+    <!--begin::Input group=-->
+    <div class="fv-row mb-8">
+        <!--begin::Email-->
+        <input type="text" placeholder="Data" wire:model="data" autocomplete="off"
+            class="form-control bg-transparent @error('data') is-invalid @enderror" />
+        @error('data')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+        @enderror
+        <!--end::Email-->
     </div>
-    <button type="submit" class="btn btn-success mt-3">
-        <i class='ki-duotone ki-check fs-1'></i>
-        Save
-    </button>
+
+    <!--begin::Submit button-->
+    <div class="d-grid mb-10">
+        <button type="submit" id="kt_sign_in_submit" class="btn btn-primary">
+            <!--begin::Indicator label-->
+            <span class="indicator-label">Save</span>
+            <!--end::Indicator label-->
+            <!--begin::Indicator progress-->
+            <span class="indicator-progress" wire:loading>
+                Please wait...
+                <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
+            <!--end::Indicator progress-->
+        </button>
+    </div>
+    <!--end::Submit button-->
 </form>
